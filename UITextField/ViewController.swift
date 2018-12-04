42a8c047aca2ca8d9f464e9142681e3c043062f7
Arshad Ali <thearshadali@yahoo.com>
2018-12-04 10:20:53 +0530

    textfield

--- UITextField/ViewController.swift
+++ UITextField/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var textField: UITextField = {
+        let tf = UITextField(frame: CGRect.zero)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Enter your name"
+        tf.allowsEditingTextAttributes = true  // when user select text and pop up appears with options style like bold ,italic
+        tf.borderStyle = .roundedRect
+        tf.becomeFirstResponder()
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(textField)
+        addConstrains()
+    }
+    
+    func addConstrains(){
+        NSLayoutConstraint.activate([textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                    textField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                                    textField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+                                    textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)])
     }
 
 
